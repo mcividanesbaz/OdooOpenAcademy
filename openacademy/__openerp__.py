c3b568f2358fb94a98f9e9b6b03e6c99ff71b28b
mcividanesbaz <Navidad1995>
2016-03-31 01:08:58 +0200

    commit 2- creado el menu y añadido al openerp

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -24,7 +24,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+      # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
